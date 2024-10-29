81df4a6c5b507545ab982719ac4c54083aaa3d3c
HaneenHamchou <haneen.hamchou@gmail.com>
2024-10-29 02:31:36 +0100

    Create Corporations model

--- dbapp/models.py
+++ dbapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create Corporation models to represent database tables .
+class Corporations(Model.models):
+	name = models.CharField(max_lenght = 100)
+	address = models.TextField()
+	url = models.URLField(null=True)
+	#relationship between Corporation Modela and User Model
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+        return self.name
