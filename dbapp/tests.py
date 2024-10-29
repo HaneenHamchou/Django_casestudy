c7ae9cb6a22a8c8d4476ee552456c4476da282a5
HaneenHamchou <haneen.hamchou@gmail.com>
2024-10-29 02:59:31 +0100

    Create test cases

--- dbapp/tests.py
+++ dbapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Corporation
 
-# Create your tests here.
+#Create a test case to verify the Corporation and User models
+class CorporationModelTest(TestCase):
+    def test_create_corporation(self):
+        Corporation.objects.create(name="Haneen Hamchou", address="Von-Merklin Str")
+        self.assertEqual(Corporation.objects.count(), 1)
