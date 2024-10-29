95a84cef36d570e901a159f98fdd3ca194177269
HaneenHamchou <haneen.hamchou@gmail.com>
2024-10-29 12:06:38 +0100

    Add User model

--- dbapp/models.py
+++ dbapp/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
 
 # Create Corporation models to represent database tables .
-class Corporations(Model.models):
-	name = models.CharField(max_lenght = 100)
+class Corporations(models.Model):
+	id = models.AutoField(null=False, primary_key=True)
+	name = models.CharField(max_length = 100)
 	address = models.TextField()
 	url = models.URLField(null=True)
-	#relationship between Corporation Modela and User Model
+	#relationship between Corporation Model and User Model
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+	class User(AbstractUser):
+    id = models.AutoField(null=False, primary_key=True)
+    name = CharField(blank=True, max_length=255)
+
+
 	def __str__(self):
-        return self.name
+		return self.name
+
